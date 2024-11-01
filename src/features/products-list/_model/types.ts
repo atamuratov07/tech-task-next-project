11eaa90dfcfc696b33debcad88cb195055a9ba16
Damir <damiratamuratov07@gmail.com>
2024-11-01 17:42:13 +0500

    fixed deploying errors

--- src/features/products-list/_model/types.ts
+++ src/features/products-list/_model/types.ts
@@ -1,3 +1,5 @@
-import { Product as DbProduct, Cart as DbCart } from '@prisma/client'
-
-export type Product = Omit<DbProduct, 'id' | 'updatedAt' | 'createdAt'>
+export type Product = {
+	title: string
+	price: number
+	imageUrl: string
+}
